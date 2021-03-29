ff521ba7e2eef9e81289a92120a00479bc6a4d1f
oishi <abdaqoopmhm@gmail.com>
2021-03-29 21:50:00 +0900

    提出2回目

--- task3/src/App.vue
+++ task3/src/App.vue
@@ -14,16 +14,16 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item, index) in searchTaskList" :value="item" :key="item.id">
-          <td>{{item.id}}</td>
-          <td>{{item.task}}</td>
-          <td><button @click="changeStatus(item)">{{item.working}}</button></td>
-          <td><button @click="deleteRow(index)">{{item.delete}}</button></td>
+        <tr v-for="(item) in searchTaskList" :value="item" :key="item.id">
+          <td> {{item.id}} </td>
+          <td> {{item.task}} </td>
+          <td><button @click="changeStatus(item)"> {{item.working}} </button></td>
+          <td><button @click="deleteRow(item)"> {{item.delete}} </button></td>
         </tr>
       </tbody>
     </table>
     <h2>新規タスクの追加</h2>
-    <input type="text" id="taskText">
+    <input type="text" id="taskText" v-model='task'>
     <input type="button" value="追加" @click="addTaskList">
   </div>
 </template>
@@ -32,6 +32,7 @@
 export default {
   data() {
     return {
+      task: '',
       item: '',
       sort: 'すべて',
       taskList: []
@@ -39,18 +40,18 @@ export default {
   },
   methods: {
     addTaskList() {
-      let text = document.getElementById("taskText");
-      this.taskList.push({id:Object.keys(this.taskList).length, task:text.value, working:'作業中', delete:'削除'})
-      text.value = '';
+      this.taskList.push( {id:Object.keys(this.taskList).length, task:this.task, working:'作業中', delete:'削除'} )
+      this.task = '';
     },
-    deleteRow(index) {
-      this.$delete(this.taskList, index);
-      for (let i=0; i<Object.keys(this.taskList).length; i++){
+    deleteRow(item) {
+      this.delId = this.taskList.indexOf(item);
+      this.taskList.splice(this.delId,1);
+      for (let i = 0; i < Object.keys(this.taskList).length; i++) {
         this.taskList[i].id = i;
       }
     },
     changeStatus(item) {
-      if (item.working === '作業中'){
+      if (item.working === '作業中') {
         item.working = '完了';
       } else {
         item.working = '作業中';
@@ -58,16 +59,16 @@ export default {
     },
   },
   computed: {
-    searchTaskList: function(){
+    searchTaskList: function() {
       let sortState = this.sort;
-      return this.taskList.filter(function(value){
-          if (sortState === 'すべて'){
+      return this.taskList.filter(function(value) {
+          if (sortState === 'すべて') {
             return true;
           }
-          else if (sortState === '作業中'){
+          else if (sortState === '作業中') {
             return value.working === '作業中'
           }
-          else if (sortState === '完了'){
+          else if (sortState === '完了') {
             return value.working === '完了'
           }
       })
